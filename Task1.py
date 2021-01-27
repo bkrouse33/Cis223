e209ce92c9d73ba5315734787374ae8c6f46160e
Alexc18O <57201754+Alexc18O@users.noreply.github.com>
2021-01-26 18:16:14 -0600

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -22,10 +22,12 @@ class Stack:
     def push(self,item):
         if len(self.items) != self.maxsize:
             self.items.append(item)
+            self.tos = self.tos + 1 
 
     def pop(self):
         if len(self.items) != 0:
-            return self.items.pop()
+            self.tos = self.tos - 1
+            return self.items.pop() 
     
     def peek(self):
         return self.items[len(self.items)-1]
@@ -38,54 +40,43 @@ class Stack:
 class Queue:
 
     def __init__(self,maxsize):
+        self.key = []
         self.maxsize = maxsize
+        self.tos = -1 
         self.stack1 = Stack(maxsize)
         self.stack2 = Stack(maxsize)
 
     def Enqueue(self,key):      # this is where we will have to loop through and transfer to one stack from anoither 
         #First we much check of this queue is full or empty or if first element 
-        if self.Rear != self.maxsize:
-            if self.Front == -1 and self.Rear == -1:    #First element is going to be added to queue
-                self.queue.append()
-                self.Front = 0
-                self.Rear = 0
-            else:
-                self.queue.append()
-                self.Front =+ 1
-        else:
-            return "is full"
+         if len(self.key) != self.maxsize:
+            self.key.append(key)
+            self.tos = self.tos + 1 
+            
 
 
     def Dequeue(self):
-        if self.Front==-1 and self.Rear == -1:
-            return "Is Empty"
+        if self.tos != -1:
+            for i in range(self.maxsize):
+                temp = self.stack1.pop()
+                self.stack2.push(temp)
+                self.stack2.pop()
+            
+        
 
-
-    def isFull(self):
-        if self.Rear == self.maxsize:
-            return True 
-        else:
-            return False
-
-    def isEmpty(self):
-        if self.Front==-1 and self.Rear == -1:
-            return True
-        else:
-            return False
 
     
 
 
+Q = Queue(5)
+Q.Enqueue(1)
+Q.Enqueue(2)
+Q.Enqueue(3)
+Q.Enqueue(4)
 
 
 
-S = Stack(5)
-S.push(1)
-S.push(12)
-S.push(6)
-S.push(17)
-S.push(100)
 
 
-#for i in range(5):
- #   print(S.pop())
+for i in range(5):
+  print(Q.Dequeue())
+
